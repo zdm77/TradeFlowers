d3d122bebd2b3c40b2707162f26d964da1592c42
zdm77@mail.ru <Dima>
2019-07-10 19:32:37 +0300

    Добавил модель и редактор

---
+++ pas/Helpers/UException.pas
@@ -0,0 +1,24 @@
+unit UException;
+
+interface
+
+uses
+  System.SysUtils, Winapi.Windows, Vcl.Forms;
+
+  procedure getExceptMessage(AExcept : Exception);
+
+implementation
+
+procedure getExceptMessage(AExcept : Exception);
+var
+  p : integer;
+begin
+
+  p := Pos(' violates foreign key constraint', AExcept.Message);
+  if (p > 0) then
+    Application.MessageBox('Имеются связанные данные. Удаление не возможно.', 'Ошибка', MB_OK + MB_ICONERROR)
+  else
+    Application.MessageBox(pchar(AExcept.Message), 'Ошибка', MB_OK + MB_ICONERROR)
+end;
+
+end.
